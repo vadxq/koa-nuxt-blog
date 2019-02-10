3af7c4cd3be85c92cef49d3dac9885aa578a4923
vadxq <dongxianlin@vadxq.com>
2019-02-10 17:38:57 +0800

    add list controls

---
+++ controls/list.js
@@ -0,0 +1,115 @@
+import mongoose from 'mongoose';
+
+const List = mongoose.model('List');
+
+export const saveArticle = async (ctx, next) => {
+  const body = ctx.request.body;
+  const info = new List(body);
+  const saveInfo = await info.save();
+
+  if (saveInfo) {
+    ctx.body = {
+      status: 1,
+      msg: saveInfo
+    };
+  } else {
+    console.log(changeInfo);
+    ctx.body = {
+      status: 1
+    };
+  };
+};
+
+// 编辑文章
+export const changeArticle = async (ctx, next) => {
+  const body = ctx.request.body;
+
+  if (body !== null && body !== ' ') {
+    const changeInfo = await List.findByIdAndUpdate({id: body.id}, {$set: {
+      title: body.title,
+      content: body.content,
+      description: body.description,
+      coverimg: body.coverimg,
+      checked: body.checked,
+      dele: body.dele
+    }}, {multi: false});
+    
+    if (changeInfo) {
+      ctx.body = {
+        status: 1,
+        msg: changeInfo
+      };
+    } else {
+      console.log(changeInfo);
+      ctx.body = {
+        status: 0
+      };
+    };
+  } else {
+    ctx.body = {
+      status: 0
+    };
+  };
+
+};
+
+// 获取已发表文章列表
+export const fetchArticle = async (ctx, next) => {
+  const info = await Info.find({
+    dele: false,
+    checked: true
+  }, {
+    _id: 1,
+    title: 1,
+    content: 1,
+    createtime: 1,
+    updatetime: 1,
+    views: 1,
+    links: 1,
+    description: 1,
+    coverimg: 1,
+  });
+
+  if (info.length) {
+    ctx.body = {
+      status: 1,
+      info: infos
+    };
+  } else {
+    console.log(changeInfo);
+    ctx.body = {
+      status: 0
+    };
+  };
+};
+
+// 获取所有文章列表
+export const fetchAllArticle = async (ctx, next) => {
+  const info = await Info.find({
+    dele: false
+  }, {
+    _id: 1,
+    title: 1,
+    content: 1,
+    createtime: 1,
+    updatetime: 1,
+    views: 1,
+    links: 1,
+    description: 1,
+    coverimg: 1,
+    checked: 1,
+    dele: 1
+  });
+
+  if (info.length) {
+    ctx.body = {
+      status: 1,
+      info: infos
+    };
+  } else {
+    console.log(changeInfo);
+    ctx.body = {
+      status: 0
+    };
+  };
+};
