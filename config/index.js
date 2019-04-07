f08d7a4f350708d59458783f76fa79b9c60371e2
vadxq <dongxianlin@vadxq.com>
2019-04-08 00:09:11 +0800

    add config

---
+++ config/index.js
@@ -0,0 +1,11 @@
+// mongoose path
+export const dbPath = '';
+
+export const opt = {
+  useNewUrlParser: true
+}
+// server port
+export const port = 7190;
+
+// jwt_secret
+export const jwt_secret = 'jwtsec'
